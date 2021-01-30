643ae605384188abf201c4a7a35e13c2ef4ea215
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-01-30 14:37:15 +0530

    Project intialised

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='myblog-ashutoshc8101',
+    version='0.0.1',
+    author='ashutoshc8101',
+    url='https://github.com/ashutoshc8101/myblog'
+
+)
